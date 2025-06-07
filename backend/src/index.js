b59c531e62a1ac5216fb6fa3f0e9f404f07d5b17
mrhairinson <158176987+mrhairinson@users.noreply.github.com>
2025-06-07 20:22:38 +0700

    feat: finish app

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./libs/db.js";
@@ -10,7 +10,8 @@ import { app, server } from "./libs/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); //Extract json data from body middleware
 app.use(cookieParser()); //Allow to parse cookie
@@ -24,6 +25,13 @@ app.use(
 
 app.use(`${process.env.API_VERSION}/auth`, authRoutes);
 app.use(`${process.env.API_VERSION}/message`, messageRoutes);
+
+if(process.env.NODE_ENv === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))) //get static index.html of dist folder of application in frontend
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));//serve frontend application in same port
+  })
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on Port:", PORT);
--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -1,6 +1,6 @@
 import axios, { type AxiosInstance } from "axios";
 
 export const axiosIns:AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
   withCredentials: true, //Allow to send cookies in each req
 });
